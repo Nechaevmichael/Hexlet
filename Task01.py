7e201f89cb42199d53a7c4ba85312a2d0deaf03c
NechaevMichael <nechaevmichael@mail.ru>
2023-04-02 18:50:00 +0500

    add Task01

---
+++ Task01.py
@@ -0,0 +1,8 @@
+#
+# Напишите программу, которая выводит на экран:
+#
+# - Did Joffrey agree?
+# - He did. He also said "I love using \n".
+# Ваша программа должна использовать только один print(), но выводить результат на экране в две строчки, как показано выше.
+
+print('- Did Joffrey agree?\n- He did. He also said "I love using \\n".')
