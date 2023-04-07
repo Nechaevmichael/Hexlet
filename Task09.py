9e7a50cfa2df6ddc60c49aaca1519dc9284ed4fe
NechaevMichael <nechaevmichael@mail.ru>
2023-04-07 17:33:42 +0500

    add Task09

---
+++ Task09.py
@@ -0,0 +1,9 @@
+# Реализуйте функцию, которая проверяет является ли переданный параметр строкой.
+
+def string_or_not(value):
+    if value == isinstance(value, str):
+        print('yes')
+    else:
+        print('no')
+
+string_or_not(3)
--- example.py
+++ example.py
@@ -1,1 +1,8 @@
-print(2018 % 100)
+def string_or_not(value):
+    if isinstance(value, str):
+        return 'yes'
+    else:
+        return 'no'
+
+
+print(string_or_not('Hexlet'))
