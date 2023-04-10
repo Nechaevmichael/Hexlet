50eb3a461484b0ad9ada76fe6aaa36bf3bd652d2
NechaevMichael <nechaevmichael@mail.ru>
2023-04-10 13:54:03 +0500

    add Task19

---
+++ Task19/Task19.py
@@ -0,0 +1,12 @@
+# Директория Task19 в файловом дереве содержит модуль kit.py. Изучите его.
+# Реализуйте функцию print_kit() и вызовите в ней все функции, которые определены в модуле kit.py.
+# Предварительно импортируйте их.
+
+from kit import show_language, say_hello, say_bye
+
+def print_kit():
+    show_language()
+    say_hello()
+    say_bye()
+
+print_kit()
